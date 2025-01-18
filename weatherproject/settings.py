f5df61a5ab33ed0aadbe9b1d4b3b0d99653eea47
mynk.chn <mynkchn.pu@gmail.com>
2025-01-18 15:18:58 +0530

    update

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'y!^f73w5xi5zi17+q&hu+krknlm*j&018xfy8xx5(a)axn0%b%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
+if DEBUG :
+    ALLOWED_HOSTS+=['127.0.0.1','localhost']
 
 
 # Application definition
